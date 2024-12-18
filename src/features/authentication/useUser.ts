673f26b5bf9ecb1996e3c198d548168b7a0ca4b7
Charles Caleb <charlesofficial9@gmail.com>
2024-12-18 15:46:52 +0100

    update

--- src/features/authentication/useUser.ts
+++ src/features/authentication/useUser.ts
@@ -11,7 +11,7 @@ export function useUser(UserId: number | undefined) {
     refetch,
   } = useQuery<useUserType | undefined>({
     queryKey: ["user"], // Cache key is specific to the UserId
-    queryFn: () => getUser(UserId!), // Fetch the user by ID
+    queryFn: () => getUser(), // Fetch the user by ID
     enabled: !!UserId, // Only fetch if UserId is defined
     staleTime: 5000 * 60 * 1000, // Cache for 5 minutes
     retry: 3, // Retry up to 3 times in case of failure
